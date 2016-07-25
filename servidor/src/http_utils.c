a8b578b335f5961e0cf918f485827564b368fcfc
Andre Dantas <andre.dantas@aker.com.br>
2016-07-25 17:23:42 -0300

    make_header : adicionando funcao que constroi cabecalho.
    Foram adicionadas as funcoes (estaticas) get_content_length, get_date e a funcao
    make_header que constroi o header e retorna um ponteiro para ele.

--- servidor/src/http_utils.c
+++ servidor/src/http_utils.c
@@ -1,13 +1,22 @@
 #include "http_utils.h"
-
-int find_end_request(char *header)
+const char *status_conection [] = 
+  { "HTTP/1.0 200 OK\r\n",
+  "HTTP/1.0 400 Bad Request\r\n",
+  "HTTP/1.0 401 Unauthorized\r\n",
+  "HTTP/1.0 403 Forbidden\r\n",
+  "HTTP/1.0 404 Not Found\r\n",
+  "HTTP/1.0 413 Request Entity Too Large\r\n",
+  "HTTP/1.0 500 Internal Server Error\r\n",
+  "HTTP/1.0 503 Service Unavailable\r\n"
+  };
+int find_end_request(char *request)
 { 
   const int found = 0;
-  if ((strstr(header, "\r\n\r\n")) != NULL)
+  if ((strstr(request, "\r\n\r\n")) != NULL)
     return found;
-  else if ((strstr(header,"\n\n")) != NULL)
+  else if ((strstr(request,"\n\n")) != NULL)
     return found;
-  else if ((strstr(header,"\n\r\n\r")) != NULL)
+  else if ((strstr(request,"\n\r\n\r")) != NULL)
     return found;
   else
     return -1;
@@ -29,4 +38,62 @@ char *get_resquest_info(char *request)
   sscanf(request,"GET %s HTTP/1.%*[0-9]\r\n\r\n %*[^|]", file_name);
   return file_name;
 }
- 
+
+static char *get_content_length(const char *file_name, unsigned long *file_size)
+{
+  struct stat st;
+  const int length_value = 12; /*Terabytes 2^12*/
+  const int content_len_size = strlen( "Content-Length: \r\n") + length_value;
+  char *content_length = (char *) malloc (content_len_size * sizeof(char));
+  if (content_length == NULL)
+    return content_length;
+  stat(file_name, &st);
+  if (st.st_size < 0)
+  {
+    snprintf(content_length, content_len_size,"Content-Length: %d\r\n",0);
+    *file_size = 0;    
+  }
+  else
+  {
+    snprintf(content_length, content_len_size,"Content-Length: %ld\r\n",
+      st.st_size);
+    *file_size = st.st_size;
+  }
+  return content_length;
+}
+static char *get_date()
+{
+  const char *weekday[] = { "Sun", "Mon", "Tue", "Thu", "Fri", "Sat"};
+  const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+    "Aug", "Sep", "Oct", "Nov", "Dec"};
+  const int date_length = strlen("Date : Mon, 25 Jul 2016 15:11:59 GMT\r\n"); 
+  char *date = (char *) malloc (date_length * sizeof(char));
+  struct tm *local;
+  time_t t;
+  t = time(NULL);
+  local = localtime(&t);
+  mktime (local); 
+  
+  snprintf(date, date_length,"Date: %s, %d %s %d %d:%d:%d GMT\r\n", 
+    weekday[local->tm_wday], local->tm_mday, months[local->tm_mon],
+    local->tm_year+1900, local->tm_hour, local->tm_min, local->tm_sec);
+  return date;
+}
+char *make_header(const char *file_name, const int status, 
+  unsigned long *file_size)
+{
+  const char content_type[] = "Content-Type: application/octet-stream\r\n";
+  const char server[] = "Server: Cacique/1.0\r\n";
+  const int end_header_len = 2;
+  char *content_length = get_content_length(file_name, file_size);
+  char *date = get_date();
+  int header_size = strlen(status_conection[status]) + strlen(content_type)+1 \
+    + strlen(content_length)+1  + strlen(date)+1 + strlen(server)+1 \
+    + end_header_len;
+ char *header = (char *) malloc (header_size * sizeof(char));
+ snprintf(header, header_size, "%s%s%s%s%s\r\n", status_conection[status],
+  content_type, content_length, date,server);
+  free(content_length);
+  free(date);
+  return header;
+}
--- servidor/src/http_utils.h
+++ servidor/src/http_utils.h
@@ -3,7 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#define OK 0
+#define BAD_REQUEST 1
+#define UNAUTHORIZED 2
+#define FORBIDDEN 3
+#define NOT_FOUND 4
+#define REQUEST_TOO_LARGE 5
+#define INTERNAL_ERROR 6
+#define SERVICE_UNAVAILABLE 7
 #define MAX_HEADER_SIZE 4000
 int find_end_request(char *header);
 char *get_resquest_info(char *request);
+char *make_header(const char *file_name, const int status, 
+  unsigned long *file_size);
 #endif /* SERVER_LIB_H */
