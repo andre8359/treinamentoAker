30a0f11d761db12bb6f57b41bf851f6e7dc84b01
Andre Dantas <andre.dantas@aker.com.br>
2016-07-27 17:42:44 -0300

    teste : adaptando script para teste de stress no servidor.

--- servidor/script_test.py
+++ servidor/script_test.py
@@ -2,33 +2,33 @@
 import sys
 import os
 from multiprocessing import Process
+import time
 
 PORT  = "8080"
-IP = "10.4.3.1"
-file_urls = [IP +':' + PORT + '/pdfs/evolucaoDosCodecs.pdf',
-            IP + ':' + PORT + '/imgs/lena.jpeg',
-            IP +':' + PORT + '/imgs/mandrill.png',
-            IP +':' + PORT + '/teste_300mb.iso',
-            IP +':' + PORT + '/teste']
+IP = "127.0.0.1"
+file_urls = IP +':' + PORT + '/teste_300mb.iso'
+#file_urls = [IP +':' + PORT + '/pdfs/evolucaoDosCodecs.pdf',
+#            IP + ':' + PORT + '/imgs/lena.jpeg',
+#            IP +':' + PORT + '/imgs/mandrill.png',
+#            IP +':' + PORT + '/teste_300mb.iso',
+#            IP +':' + PORT + '/teste']
 process_list = []
 
-def download_file(file_url, fileout_name):
-  print('Download start --->' + fileout_name)
-  os.system('curl -sS --http1.0 --url ' + file_url + ' -o arquivos_download/' 
-    + fileout_name)
-  print('Download end --->' + fileout_name)
-for i in xrange(len(file_urls)):
+def download_file(file_url, fileout_name, i):
+  print('Download start ---> '+ str(i)+ ' - ' + fileout_name)
+  os.system('wget -nc  ' + file_url + ' -O arquivos_download/' 
+    + fileout_name + "_" + str(i) )
+  print('#### Download end ---> '+ str(i) +' - '+ fileout_name + " ")  
+#os.system('curl -s --url 10.4.3.1:8080/teste_4gb.iso -o teste_4gb_ini.iso')
+for i in range(0,20):
   print("-----------------------------------------------------")
   p = Process(target=download_file,
-args=(file_urls[i],file_urls[i][file_urls[i].rfind('/')+1:], ))
+args=(file_urls,file_urls[file_urls.rfind('/')+1:],i, ))
   p.start()
-  process_list.append(p)
-  print("-----------------------------------------------------")
-for p in process_list:
-    p.join()
-for i in xrange(len(file_urls)):
-  print("*******************************************************")
-  os.system("md5sum root/" + file_urls[i][file_urls[i].find('/')+1:]
-    + " arquivos_download/" + file_urls[i][file_urls[i].rfind('/')+1:])
+  print("   #################  %d  ####################" % i)
+#for i in xrange(len(file_urls)):
+print("*******************************************************")
+#  os.system("md5sum root/" + file_urls[i][file_urls[i].find('/')+1:]
+ #   + " arquivos_download/" + file_urls[i][file_urls[i].rfind('/')+1:])
 print("*******************************************************")
 
